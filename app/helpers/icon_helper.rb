23cac508975a2e8ee60772738e4b643b729cdb19
Bruno Cavalcante <bruno.cavalcante@gocase.com.br>
2019-02-25 17:30:41 -0300

    edit icon + more icon behaviors

--- app/helpers/icon_helper.rb
+++ app/helpers/icon_helper.rb
@@ -3,4 +3,13 @@ module IconHelper
     return if !checked
     content_tag :i, 'done', class: 'material-icons check-icon'
   end
+
+  def edit_icon(text = 'Edit')
+    content_tag :div, class: 'btn btn--transparent btn--nano btn--block' do
+      content_tag :div, class: 'flexbox flexbox--vcentered' do
+        concat content_tag :i, 'edit', class: 'material-icons icon icon--left icon--small'
+        concat text
+      end
+    end
+  end
 end
