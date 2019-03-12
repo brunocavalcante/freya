243bf08c8122b8b9c2d042a0e16b583ef8ad4784
Bruno Cavalcante <bruno.cavalcante@gocase.com.br>
2019-03-12 13:46:40 -0300

    new ui for destroy links

--- app/helpers/toolbar_helper.rb
+++ app/helpers/toolbar_helper.rb
@@ -9,11 +9,20 @@ module ToolbarHelper
   end
 
   def destroy_link(url, name, options = {})
-    link_to url, class: 'btn btn--danger btn--iconable', method: :delete, data: { confirm: I18n.t(:delete_confirm, scope: 'freya.toolbar') } do 
+    link_to url, class: 'btn btn--destroy btn--iconable btn--nano', method: :delete, data: { confirm: I18n.t(:delete_confirm, scope: 'freya.toolbar') } do 
       content_tag :div, class: 'flexbox flexbox--vcentered' do
         concat content_tag :i, 'delete', class: 'material-icons icon icon--left flexbox__fixed'
         concat name
       end
+    end
+  end
+
+  def destroy_block(url, name, options = {})
+    options[:class] ||= ''
+    options[:class] += ' btn--nano'
+    content_tag :div, class: 'destroy-block' do 
+      concat content_tag :p, 'Not happy with this object?', class: 'paragraph--small'
+      concat destroy_link url, name, options
     end
   end
 end
